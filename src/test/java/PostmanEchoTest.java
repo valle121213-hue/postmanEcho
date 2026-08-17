57ba0b9efc4d5b6960e78bd8642f7b0881fc72a2
Valentina <angel1212@list.ru>
2026-08-17 11:51:40 +0400

    code reformat

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -7,11 +7,15 @@ public class PostmanEchoTest {
 
     @Test
     void shouldReturnSentData() {
+        // Given - When - Then
+        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
                 .when()
                 .post("/post")
+                // Проверки
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"));
